532d77a2a47e96f434d5f2882e2ad6b2ac23dbeb
fage1151 <fage1151@gmail.com>
2017-08-02 17:26:08 +0800

    修改：     Events/EventInterface.php

--- Events/EventInterface.php
+++ Events/EventInterface.php
@@ -30,32 +30,11 @@ interface EventInterface
     const EV_WRITE = 2;
 
     /**
-     * Except event
-     *
-     * @var int
-     */
-    const EV_EXCEPT = 3;
-
-    /**
-     * Signal event.
-     *
-     * @var int
-     */
-    const EV_SIGNAL = 4;
-
-    /**
      * Timer event.
      *
      * @var int
      */
     const EV_TIMER = 8;
-
-    /**
-     * Timer once event.
-     *
-     * @var int
-     */
-    const EV_TIMER_ONCE = 16;
 
     /**
      * Add event listener to event loop.
